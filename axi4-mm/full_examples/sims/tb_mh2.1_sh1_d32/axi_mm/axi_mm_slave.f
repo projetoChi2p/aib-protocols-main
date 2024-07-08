4d74fdbf79da0d9d633c8ed3828bf8a70550473d
gabcouto <gabrieliervolinof@gmail.com>
2024-07-08 14:40:42 -0300

    Added 32-bit AXI option

---
+++ axi4-mm/full_examples/sims/tb_mh2.1_sh1_d32/axi_mm/axi_mm_slave.f
@@ -0,0 +1,10 @@
+// Generated Files
+/home/couto/june/integration/mesa_trab_aib/dependencies/implementation/rtl/vendor/aib-protocols-main/axi4-mm/full_examples/sims/tb_mh2.1_sh1_d32/axi_mm/axi_mm_slave_top.sv   
+/home/couto/june/integration/mesa_trab_aib/dependencies/implementation/rtl/vendor/aib-protocols-main/axi4-mm/full_examples/sims/tb_mh2.1_sh1_d32/axi_mm/axi_mm_slave_concat.sv
+/home/couto/june/integration/mesa_trab_aib/dependencies/implementation/rtl/vendor/aib-protocols-main/axi4-mm/full_examples/sims/tb_mh2.1_sh1_d32/axi_mm/axi_mm_slave_name.sv  
+
+// Logic Link files
+-f ${PROJ_DIR}/llink/rtl/llink.f
+
+// Common Files
+-f ${PROJ_DIR}/common/rtl/common.f
